e20f2d2f57b081e0df88c37816c451f28cc529d5
Vasco2002 <manuelinho2002@gmail.com>
2024-03-27 16:34:30 +0000

    Tipagem do dataset

---
+++ TP1/tipagem.py
@@ -0,0 +1,69 @@
+import csv
+
+def ler_csv(nome_arquivo):
+    with open(nome_arquivo, 'r', newline='', encoding='utf-8') as arquivo:
+        return list(csv.reader(arquivo))
+
+def escrever_csv(nome_arquivo, dados):
+    with open(nome_arquivo, 'w', newline='', encoding='utf-8') as arquivo:
+        escritor_csv = csv.writer(arquivo)
+        escritor_csv.writerows(dados)
+
+# Processamento e Tipagem dos Dados já existentes no arquivo CSV
+def processar_dados(data, boosters, negadores):
+    resultado = []
+    for linha in data:
+        elemento = linha[0]
+        intensidade = linha[1] if len(linha) > 1 else "0"  #caso não tenha intensidade passa a ser 0
+        tipo = linha[2] if len(linha) > 2 else ""
+        
+        # Dar tipo INCR ou DECR aos boosters
+        if elemento in boosters:
+            tipo = boosters[elemento]
+        
+        # Dar tipo NEG aos negadores
+        if elemento in negadores:
+            tipo = "NEG"
+        
+        # Dar tipo TERM aos termos e tirar o tipo TBD aos elementos que não têm tipo
+        if tipo == "TBD":
+            if len(elemento.split()) > 1:
+                tipo = "TERM"
+            else:
+                tipo = ""
+        
+        linha_processada = [elemento, intensidade, tipo]
+        resultado.append(linha_processada)
+    
+    return resultado
+
+data = ler_csv('sentiment.csv')
+
+# Processar negadores
+negate = ler_csv('datasets/negate.txt')
+elementos_negativos = set(linha[0] for linha in negate)
+elementos_data = set(linha[0] for linha in data)
+# Adicionar elementos de negate.txt que não estão em sentiment.csv
+for elemento in elementos_negativos:
+    if elemento not in elementos_data:
+        data.append([elemento, "0"])
+
+# Processar boosters
+booster = ler_csv('datasets/booster.txt')
+elementos_booster = {}
+for linha in booster:
+    partes = linha[0].rsplit(' ', 1)
+    elemento = partes[0]
+    tipo = partes[1] if len(partes) > 1 else ""
+    elementos_booster[elemento] = tipo
+
+# Processar os dados
+resultado = processar_dados(data, elementos_booster, elementos_negativos)
+
+# Adicionar elementos de booster.txt que não estão em sentiment.csv
+elementos_data = set(linha[0] for linha in data)
+for elemento, tipo in elementos_booster.items():
+    if elemento not in elementos_data:
+        resultado.append([elemento, "0", tipo])
+
+escrever_csv('final.csv', resultado)
