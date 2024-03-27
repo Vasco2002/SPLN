f0d3b310a0246ef9454ceac1860e19a5d4cb7438
jmfl27 <73397910+jmfl27@users.noreply.github.com>
2024-03-27 17:39:32 +0000

    dataset do lexPT acrescentado à geração

--- TP1/generateDataset.py
+++ TP1/generateDataset.py
@@ -2,6 +2,8 @@ import os
 import re
 import pprint
 import csv
+import random
+import sys
 
 def getFilesInDirectory(directory):
     filePaths = []
@@ -18,11 +20,50 @@ def printFiles(filesList):
 def leia(file,dataset):
     with open(file, 'r') as f:
         for line in f:
-            data = re.split(r'^([\w\s\W-]+?)\s+([-+]?\d*\.?\d+)(?:\s+\[.*\])?', line.strip())
-            #print(data[1] + ' : ' + str(data[2]))
-            if data[0] not in dataset.keys():
-                #TODO: TBD -> Tipo
-                dataset[data[1]] = [data[1],float(data[2]),"TBD"]
+            if line != '':
+                data = re.split(r'^([\w\s\W-]+?)\s+([-+]?\d*\.?\d+)(?:\s+\[.*\])?', line.strip())
+                #print(data[1] + ' : ' + str(data[2]))
+                if data[1] not in dataset.keys():
+                    #TODO: -> Tipo
+                    dataset[data[1]] = [data[1],float(data[2]),""]
+
+def lexPT(file,dataset):
+    # -4 a -0.1 | 0.1 a 4
+    minN = -1.3
+    maxN = -0.5
+    minP = 1.3
+    maxP = 0.5
+
+    with open(file, 'r') as f:
+        for line in f:
+            if line != '':
+                data = re.split(r'^([\w\s\W-]+?)\s+(\w+)', line.strip())
+                #print(data)
+                #print(data[1] + ' : ' + str(data[2]))
+                if data[1] not in dataset.keys():
+                    # Ativa polaridade mais agravada para palavrões e insultos
+                    if data[1] == 'cú':
+                        #print('MODO PALAVRAO')
+                        minN = -3.4
+                        maxN = -2.2
+
+                    # Caso de exceção de trepa e trepar
+                    if 'trepa' in data[1]:
+                        #print('ENCONTREI TREPAR')
+                        dataset[data[1]] = [data[1],0.6,""]
+                    elif data[2] == 'POSITIVE' or data[2] == 'POSITIVO' or data[2] == 'POSITIV':
+                        #print(data[2] + ': ' + data[1])
+                        pol = round(random.uniform(minP, maxP), 1)
+                        dataset[data[1]] = [data[1],pol,""]
+                    elif data[2] == 'NEGATIVE' or data[2] == 'NEGATIVO':
+                        #print(data[2] + ': ' + data[1])
+                        pol = round(random.uniform(minN, maxN), 1)
+                        dataset[data[1]] = [data[1],pol,""]
+                    else:
+                        print(data[2])
+                        sys.exit()
+
+                    #print(dataset[data[1]])
 
 def makeCSV(dataset,rowList):
     for pal in dataset:
@@ -42,8 +83,11 @@ rowList = [["palavra","polaridade","tipo"]]
 for file in filesList:
     print(file.replace(directoryPath+"/",""))
     if 'vader_lexicon' in file:
-        leia(file,dataset)
-        pprint.pprint(dataset)
+        leia(file,dataset)   
 
+    if "lex_pt" in file:
+        lexPT(file,dataset)
+
+pprint.pprint(dataset)
 
 makeCSV(dataset,rowList)
